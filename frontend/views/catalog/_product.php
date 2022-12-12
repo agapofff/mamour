31ca352685595dcb7bb5cab20301e297dde97cbe
Dmitriy Agapofff <agapofff@gmail.com>
2022-12-12 04:27:34 +0300

    Update _product.php

--- frontend/views/catalog/_product.php
+++ frontend/views/catalog/_product.php
@@ -4,25 +4,25 @@
     use yii\helpers\HtmlPurifier;
 ?>
 
-    <div class="card bg-transparent border-0 product">
-        <div class="card-body position-relative p-0">
-            <a href="<?= Url::to(['/product/' . $product->slug]) ?>">
-                <?php
-                    $image = $product->getImage();
-                    $cachedImage = '/images/cache/Product/Product' . $image->itemId . '/' . $image->urlAlias . '_x500.' . $image->extension;
-                ?>
-                <img data-src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImage) ? $cachedImage : $image->getUrl('x500') ?>" class="img-fluid lazyload" alt="<?= $image->alt ? $image->alt : $productName ?>" loading="lazy">
-            </a>
-            <p class="text-center montserrat font-weight-bold mt-1_5 mb-0_5">
-                <?= $productName ?>
-            </p>
-            <p class="price text-center montserrat font-weight-light">
-            <?php if ($oldPrice) { ?>
-                <del class="text-muted d-none"><?= Yii::$app->formatter->asCurrency($oldPrice, Yii::$app->params['currency']) ?></del>&nbsp;
-            <?php } ?>
-            <?php if ($price) { ?>
-                <?= Yii::$app->formatter->asCurrency($price, Yii::$app->params['currency']) ?>
-            <?php } ?>
-            </p>
-        </div>
+<div class="card bg-transparent border-0 product">
+    <div class="card-body position-relative p-0">
+        <a href="<?= Url::to(['/product/' . $product->slug]) ?>">
+            <?php
+                $image = $product->getImage();
+                $cachedImage = '/images/cache/Product/Product' . $image->itemId . '/' . $image->urlAlias . '_x500.' . $image->extension;
+            ?>
+            <img data-src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImage) ? $cachedImage : $image->getUrl('x500') ?>" class="img-fluid lazyload" alt="<?= $image->alt ? $image->alt : $productName ?>" loading="lazy">
+        </a>
+        <p class="text-center montserrat font-weight-bold mt-1_5 mb-0_5">
+            <?= $productName ?>
+        </p>
+        <p class="price text-center montserrat font-weight-light">
+        <?php if ($oldPrice) { ?>
+            <del class="text-muted d-none"><?= Yii::$app->formatter->asCurrency($oldPrice, Yii::$app->params['currency']) ?></del>&nbsp;
+        <?php } ?>
+        <?php if ($price) { ?>
+            <?= Yii::$app->formatter->asCurrency($price, Yii::$app->params['currency']) ?>
+        <?php } ?>
+        </p>
     </div>
+</div>
