978897e18d31fc9d43a19110e4af4b1d6be93641
Dmitriy Agapofff <agapofff@gmail.com>
2022-12-19 04:05:11 +0300

    Update _form.php

--- vendor/dvizh/yii2-shop/src/views/product/_form.php
+++ vendor/dvizh/yii2-shop/src/views/product/_form.php
@@ -456,6 +456,7 @@ use PELock\ImgOpt\ImgOpt;
                                 [
                                     'class' => EditableColumn::className(),
                                     'attribute' => 'sku',
+                                    'format' => 'raw',
                                     'url' => ['/shop/modification/edit-field'],
                                     // 'type' => 'text',
                                     'label' => Yii::t('back', 'Артикул'),
